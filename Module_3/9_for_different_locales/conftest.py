f35df4585660551b737a7adc9f4b9bd3b43af5a8
doctorwait <prosnites@yandex.ru>
2023-01-10 00:30:54 +0400

    Готово задание для проверки другими участниками

--- Module_3/9_for_different_locales/conftest.py
+++ Module_3/9_for_different_locales/conftest.py
@@ -1,20 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 import pytest
-
+import os
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='ru',
                      help="Выберите локаль (на каком языке).")
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
-    #options = Options()
-    #options.add_experimental_option(
-    #    'prefs', {'intl.accept_languages': user_language})
-    options_firefox = OptionsFirefox()
+    options_firefox = Options()
+    options_firefox.binary_location = '/snap/firefox/2154/usr/lib/firefox/firefox'
     options_firefox.set_preference("intl.accept_languages", user_language)
     browser = webdriver.Firefox(options=options_firefox)
     yield browser
--- Module_3/9_for_different_locales/test_items.py
+++ Module_3/9_for_different_locales/test_items.py
@@ -1,1 +1,18 @@
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
+link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_backet_button_is_exist(browser):
+    browser.get(link)
+    try:
+        WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "btn-primary")))
+    except TimeoutException:
+        print('Элемент не был найден.')
+
+if __name__ == '__main__':
+    test_backet_button_is_exist()
+    # Заметим, что PyCharm подсвечивает жёлтым скобочки, что мол не хватает аргумента. Но всё работает благодаря
+    # conftest, как при запуске из терминала, так и напрямую из файла.
