d5f76253e3e30d5dbddf8aa9c07eb7f41a3cc5c4
doctorwait <prosnites@yandex.ru>
2023-01-25 21:51:33 +0400

    Methods that is checking some non-presented element were added in Base Class

--- Module_4/pages/base_page.py
+++ Module_4/pages/base_page.py
@@ -1,8 +1,9 @@
-import time
-
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 import math
+import time
 
 
 class BasePage:
@@ -26,6 +27,29 @@ class BasePage:
 
     def find_list_of_elements(self, by, value):
         return self.driver.find_elements(by=by, value=value)
+
+    def is_not_element_present(self, how, what, timeout=4):
+        """
+        Проверяет, что элемент не появляется на странице в течение заданного времени. Упадет, как только увидит искомый
+        элемент. Не появился: успех, тест зеленый.
+        """
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        """
+        Когда мы хотим проверить, что какой-то элемент исчезает. Будет ждать до тех пор, пока элемент не исчезнет.
+        """
+        try:
+            WebDriverWait(self.driver, timeout, 1, (TimeoutException, )).until_not(
+                EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
 
     def solve_quiz_and_get_code(self):
         alert = self.driver.switch_to.alert
